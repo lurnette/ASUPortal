599f5a6628659c76b860baa34bea987343cfa600
lurnette <lurnette@mail.ru>
2013-03-25 22:59:41 +0600

    Update groups.php

--- _modules/_acl_manager/groups.php
+++ _modules/_acl_manager/groups.php
@@ -4,6 +4,9 @@
  * Ну да ладно, это еще не так страшно, как до этого было
  */
 require_once("../../core.php");
+/*
+* Конструкция однократного включения require_once 
+*/
 mysql_query("SET NAMES UTF8");
 
 $controller = new CACLGroupController();
