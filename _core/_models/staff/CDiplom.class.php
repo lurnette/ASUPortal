37353a7a7e6a2997c05b5c806c0bb3250be92631
Ekaterina Biryukova <lurnette@mail.ru>
2013-03-28 20:40:39 +0600

    Testik

--- _core/_models/staff/CDiplom.class.php
+++ _core/_models/staff/CDiplom.class.php
@@ -7,7 +7,7 @@
  * To change this template use File | Settings | File Templates.
  */
 class CDiplom extends CActiveModel {
-    protected $_table = TABLE_DIPLOMS;
+    protected $_table = TABLE_DIPLOMS; // protected (защищенный) разрешает доступ наследуемым и родительским классам
     protected $_mark = null;
     protected $_student = null;
     protected $_person = null;
@@ -17,7 +17,7 @@ class CDiplom extends CActiveModel {
     protected $_confirmation = null;
     protected $_practPlace = null;
     protected $_reviewer = null;
-    protected function relations() {
+    protected function relations() { // метод relations проверяет наличие связей с другими записями
         return array(
             "mark" => array(
                 "relationPower" => RELATION_HAS_ONE,
@@ -84,25 +84,25 @@ class CDiplom extends CActiveModel {
     }
     public function attributeLabels() {
         return array(
-            "diplom_confirm" => "Утвержден",
-            "dipl_name" => "Тема диплома",
-            "pract_place" => "Место практики",
-            "pract_place_id" => "Место практики",
-            "kadri_id" => "Дипломный руководитель",
-            "student_id" => "Студент",
-            "group_id" => "Группа",
-            "diplom_preview" => "Дата предзащиты",
-            "date_act" => "Дата защиты",
-            "foreign_lang" => "Иностранный язык",
-            "protocol_2aspir_id" => "Протокол рекомендации в асипрантуру",
-            "recenz_id" => "Рецензент",
-            "study_mark" => "Оценка",
-            "gak_num" => "Номер ГАК",
-            "comment" => "Комментарий",
-            "diplom_number" => "Номер диплома",
-            "diplom_regnum" => "Регистрационный номер",
-            "diplom_regdate" => "Дата решения ГАК",
-            "diplom_issuedate" => "Дата выдачи"
+            "diplom_confirm" => "РЈС‚РІРµСЂР¶РґРµРЅ",
+            "dipl_name" => "РўРµРјР° РґРёРїР»РѕРјР°",
+            "pract_place" => "РњРµСЃС‚Рѕ РїСЂР°РєС‚РёРєРё",
+            "pract_place_id" => "РњРµСЃС‚Рѕ РїСЂР°РєС‚РёРєРё",
+            "kadri_id" => "Р”РёРїР»РѕРјРЅС‹Р№ СЂСѓРєРѕРІРѕРґРёС‚РµР»СЊ",
+            "student_id" => "РЎС‚СѓРґРµРЅС‚",
+            "group_id" => "Р“СЂСѓРїРїР°",
+            "diplom_preview" => "Р”Р°С‚Р° РїСЂРµРґР·Р°С‰РёС‚С‹",
+            "date_act" => "Р”Р°С‚Р° Р·Р°С‰РёС‚С‹",
+            "foreign_lang" => "Р�РЅРѕСЃС‚СЂР°РЅРЅС‹Р№ СЏР·С‹Рє",
+            "protocol_2aspir_id" => "РџСЂРѕС‚РѕРєРѕР» СЂРµРєРѕРјРµРЅРґР°С†РёРё РІ Р°СЃРёРїСЂР°РЅС‚СѓСЂСѓ",
+            "recenz_id" => "Р РµС†РµРЅР·РµРЅС‚",
+            "study_mark" => "РћС†РµРЅРєР°",
+            "gak_num" => "РќРѕРјРµСЂ Р“РђРљ",
+            "comment" => "РљРѕРјРјРµРЅС‚Р°СЂРёР№",
+            "diplom_number" => "РќРѕРјРµСЂ РґРёРїР»РѕРјР°",
+            "diplom_regnum" => "Р РµРіРёСЃС‚СЂР°С†РёРѕРЅРЅС‹Р№ РЅРѕРјРµСЂ",
+            "diplom_regdate" => "Р”Р°С‚Р° СЂРµС€РµРЅРёСЏ Р“РђРљ",
+            "diplom_issuedate" => "Р”Р°С‚Р° РІС‹РґР°С‡Рё"
         );
     }
 
@@ -113,8 +113,8 @@ class CDiplom extends CActiveModel {
         if (is_null($this->_previews)) {
             $this->_previews = new CArrayList();
             /**
-             * Если есть ключ диплома, то ищем по нему,
-             * если нету, то ищем по студенту
+             * Р•СЃР»Рё РµСЃС‚СЊ РєР»СЋС‡ РґРёРїР»РѕРјР°, С‚Рѕ РёС‰РµРј РїРѕ РЅРµРјСѓ,
+             * РµСЃР»Рё РЅРµС‚Сѓ, С‚Рѕ РёС‰РµРј РїРѕ СЃС‚СѓРґРµРЅС‚Сѓ
              */
             foreach (CActiveRecordProvider::getWithCondition(TABLE_DIPLOM_PREVIEWS, "diplom_id = ".$this->getId())->getItems() as $item) {
                 $preview = new CDiplomPreview($item);
@@ -133,7 +133,7 @@ class CDiplom extends CActiveModel {
     }
 
     /**
-     * Дата последнего предпросмотра диплома в формате unix timestamp
+     * Р”Р°С‚Р° РїРѕСЃР»РµРґРЅРµРіРѕ РїСЂРµРґРїСЂРѕСЃРјРѕС‚СЂР° РґРёРїР»РѕРјР° РІ С„РѕСЂРјР°С‚Рµ unix timestamp
      *
      * @return int
      */
